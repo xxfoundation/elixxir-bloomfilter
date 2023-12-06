eb153bf2a1b134aacae2017e1edb6dc4c39d390c
Richard T. Carback III <rick.carback@gmail.com>
2023-12-06 00:39:37 +0000

    go fmt

--- doc.go
+++ doc.go
@@ -5,10 +5,9 @@
 /*
 Package ring provides a high performance and thread safe bloom filter.
 
-License
+# License
 
 Copyright (c) 2019 Tanner Ryan. All rights reserved. Use of this source code is
 governed by a BSD-style license that can be found in the LICENSE file.
-
 */
 package ring
